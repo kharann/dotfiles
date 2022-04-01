e30c887fbf821969a183930e841a8d121b289f75
kharann <anhkhav@gmail.com>
2022-04-01 22:59:40 +0200

    more doots

--- dot_bashrc
+++ dot_bashrc
@@ -7,3 +7,4 @@
 
 alias ls='ls --color=auto'
 PS1='[\u@\h \W]\$ '
+. "$HOME/.cargo/env"
---
+++ private_dot_config/rofi/launch.sh
@@ -0,0 +1,1 @@
+G_MESSAGES_DEBUG=Dialogs.DRun rofi -show drun -drun-display-format {name} -theme config -filter "$1" 
